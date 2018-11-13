4460e2ee1ef54c19ec09c6f4b124b25330ed17c5
Hebert Porto <hebertporto@gmail.com>
2018-11-12 22:17:12 -0600

    add more init config, added nvmrc

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+
 import './App.scss';
 
 class App extends Component {
   render() {
+    const obj = {
+      a: '1',
+      b: '2',
+    };
+    const a = { c: '2', ...obj };
+
     return (
       <div className="App">
         <header className="App-header">
